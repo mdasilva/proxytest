aa304a9e62ab3de69dbecc51e69a5faa60919055
Michael Da Silva <madasilva@gmail.com>
2018-05-01 09:22:30 -0400

    adding inline documentation

--- cmd/check.go
+++ cmd/check.go
@@ -30,7 +30,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-// create wait group to track http check progress
+// Create a wait group to track asynchronous HTTP checks
 var wg sync.WaitGroup
 
 // checkCmd represents the check command
@@ -40,97 +40,17 @@ var checkCmd = &cobra.Command{
 	Long: `Perform a GET request to determine connectivity through a web proxy.
 URLs must be properly formatted with preceeding protocol.
 Provide multiple URLs as additional arguments.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Set desired logging level
-		level, err := log.ParseLevel(viper.GetString("log-level"))
-		if err != nil {
-			log.Panicln(err)
-		}
-		log.SetLevel(level)
-		if len(args) == 0 {
-			log.Fatalln("Need a least one URL to check")
-		}
-
-		// Test TCP connectivity to web proxy
-		proxy, err := checkProxy(viper.GetString("proxy-url"))
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Scrub invalid URLs
-		validURLs := parseURLs(args)
-		log.Infof("URLs to check: %d", len(validURLs))
-
-		// Create HTTP client
-		client := &http.Client{
-			Transport: &http.Transport{
-				Proxy: http.ProxyURL(proxy),
-			},
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			},
-		}
-
-		// Create channel to receive check results
-		results := make(chan *http.Response)
-		done := make(chan bool)
-
-		// Establish waitgroup size
-		wg.Add(len(validURLs))
-
-		// Begin HTTP checks
-		for _, u := range validURLs {
-			log.Debugln("Firing URL check")
-			c := HttpCheckEntry{
-				HttpClient: client,
-				URL:        u,
-				WaitGroup:  &wg,
-				Timeout:    3 * time.Second,
-				Results:    results,
-			}
-			go c.Check()
-		}
-
-		// Watch for goruntine completion
-		go func() {
-			defer close(done)
-			log.Debugln("Waiting for all URL checks to complete")
-			wg.Wait()
-		}()
-
-	resultloop:
-		for {
-			select {
-			case res := <-results:
-				log.Infof("Request: %s, Status: %s, Redirect: %s",
-					res.Request.URL.String(),
-					res.Status,
-					res.Header.Get("Location"))
-				j, err := json.Marshal(HttpCheckResults{
-					Request:  res.Request.URL.String(),
-					Status:   res.Status,
-					Redirect: res.Header.Get("Location"),
-				})
-				if err != nil {
-					log.Fatalln(err)
-				}
-				// Output JSON to stdout
-				fmt.Println(string(j))
-			case <-done:
-				break resultloop
-			}
-		}
-
-	},
+	Run: cmdMain,
 }
 
-// Display format struct
+// HttpCheckResults is the format for result output
 type HttpCheckResults struct {
 	Request  string `json:"request"`
 	Status   string `json:"status"`
 	Redirect string `json:"redirect,omitempty"`
 }
 
+// HttpCheckEntry is a HTTP check item
 type HttpCheckEntry struct {
 	HttpClient *http.Client
 	URL        url.URL
@@ -139,20 +59,22 @@ type HttpCheckEntry struct {
 	Results    chan *http.Response
 }
 
+// Check performs a HTTP GET request through the HttpClient
+// The HttpClient should be configure using a transport with a proxy server defined
 func (c *HttpCheckEntry) Check() {
 	defer c.WaitGroup.Done()
 
-	// build a http request
+	// Build the  HTTP request
 	req, err := http.NewRequest("GET", c.URL.String(), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// set a context
+	// Set a context timeout
 	ctx, cancel := context.WithTimeout(req.Context(), c.Timeout)
 	defer cancel()
 
-	// prepare and execute getk:w
+	// Prepare and execute HTTP request
 	req = req.WithContext(ctx)
 	res, err := c.HttpClient.Do(req)
 	if err != nil {
@@ -160,10 +82,10 @@ func (c *HttpCheckEntry) Check() {
 		return
 	}
 
-	// add results to channel
+	// Add HTTP GET results to channel
 	c.Results <- res
 
-	// follow redirects recursively
+	// Follow HTTP redirects recursively
 	if location := res.Header.Get("Location"); location != "" {
 		log.Debugf("Following redirect %s", location)
 		wg.Add(1)
@@ -183,6 +105,92 @@ func (c *HttpCheckEntry) Check() {
 	}
 }
 
+// cmdMain is the primary procedure for the check command execution
+func cmdMain(cmd *cobra.Command, args []string) {
+	// Set desired logging level
+	level, err := log.ParseLevel(viper.GetString("log-level"))
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.SetLevel(level)
+	if len(args) == 0 {
+		log.Fatalln("Need a least one URL to check")
+	}
+
+	// Test TCP connectivity to web proxy
+	proxy, err := checkProxy(viper.GetString("proxy-url"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Scrub invalid URLs
+	validURLs := parseURLs(args)
+	log.Infof("URLs to check: %d", len(validURLs))
+
+	// Create a shared HTTP client
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxy),
+		},
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	// Create channel to receive check results
+	results := make(chan *http.Response)
+	done := make(chan bool)
+
+	// Establish waitgroup size
+	wg.Add(len(validURLs))
+
+	// Initiate asynchronous HTTP checks
+	for _, u := range validURLs {
+		log.Debugln("Firing URL check")
+		c := HttpCheckEntry{
+			HttpClient: client,
+			URL:        u,
+			WaitGroup:  &wg,
+			Timeout:    3 * time.Second,
+			Results:    results,
+		}
+		go c.Check()
+	}
+
+	// Watch for asynchronous HTTP check completion
+	go func() {
+		defer close(done)
+		log.Debugln("Waiting for all URL checks to complete")
+		wg.Wait()
+	}()
+
+resultloop: // Handle asynchronous HTTP check results as they are returned
+	for {
+		select {
+		case res := <-results:
+			log.Infof("Request: %s, Status: %s, Redirect: %s",
+				res.Request.URL.String(),
+				res.Status,
+				res.Header.Get("Location"))
+			j, err := json.Marshal(HttpCheckResults{
+				Request:  res.Request.URL.String(),
+				Status:   res.Status,
+				Redirect: res.Header.Get("Location"),
+			})
+			if err != nil {
+				log.Fatalln(err)
+			}
+			// Output JSON to stdout
+			fmt.Println(string(j))
+		case <-done:
+			break resultloop
+		}
+	}
+
+}
+
+// parseURLs converts a slice of URL strings into a slice of url.URL objects
+// Invalid URLs are dropped. A slice of url.URLs are retruned.
 func parseURLs(args []string) []url.URL {
 	c := make([]url.URL, 0)
 	for _, a := range args {
@@ -199,6 +207,8 @@ func parseURLs(args []string) []url.URL {
 	return c
 }
 
+// checkProxy takes a URL string and tests for basic TCP connectivy
+// It returns a pointer to a url.URL object and error
 func checkProxy(proxyURL string) (*url.URL, error) {
 	u, err := url.Parse(proxyURL)
 	if err != nil {
